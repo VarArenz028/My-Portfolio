362ca66f49bcd428df8d573e242897320b69a7a3
VarArenz028 <VarArenz028@gmail.com>
2018-06-16 18:42:41 +0800

    Layouts homepage, added banners

--- src/app/entities/home/home.component.ts
+++ src/app/entities/home/home.component.ts
@@ -9,7 +9,31 @@ export class HomeComponent implements OnInit {
 
   constructor(){}
 
+  public projects = [
+    {
+      'title' : 'An Intranet Employee Portal For Sterling Global Call Center',
+      'image' : '../../../assets/imgs/illustration.jpg'
+    },
+    {
+      'title': 'A Resource Center Website for Information Systems Students in New Era University',
+      'image': '../../../assets/imgs/illustration.jpg'
+    },
+    {
+      'title': 'A Online Shopping Website for Woman Fashion(Delux)',
+      'image': '../../../assets/imgs/illustration.jpg'
+    },
+    {
+      'title': 'Intranet Employee Portal Intranet Employee Portal Intranet Employee Portal',
+      'image': '../../../assets/imgs/illustration.jpg'
+    },
+  ]
+
   ngOnInit() {
+  }
+
+  viewProject(title : string)
+  {
+      console.log(title);
   }
 
 }
