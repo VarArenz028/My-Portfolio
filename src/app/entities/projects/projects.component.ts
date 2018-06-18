1d1e4c88f05969ef29f340449895ca1410c8830b
VarArenz028 <VarArenz028@gmail.com>
2018-06-18 14:35:52 +0800

    commit

--- src/app/entities/projects/projects.component.ts
+++ src/app/entities/projects/projects.component.ts
@@ -14,22 +14,22 @@ export class ProjectsComponent implements OnInit {
     {
       'id' : 1,
       'title': 'An Intranet Employee Portal For Sterling Global Call Center',
-      'image': '../../../assets/imgs/intranetPortal/dashboard.JPG'
+      'image': './assets/imgs/intranetPortal/dashboard.JPG'
     },
     {
       'id' : 2,
       'title': 'A Resource Center Website for Information Systems Students in New Era University',
-      'image': '../../../assets/imgs/isplanning/subjects.JPG'
+      'image': './assets/imgs/isplanning/subjects.JPG'
     },
     {
       'id' : 3,
       'title': 'A Online Shopping Website for Woman Fashion(Delux)',
-      'image': '../../../assets/imgs/delux/home.JPG'
+      'image': './assets/imgs/delux/home.JPG'
     },
     {
       'id' : 4,
       'title': 'Slice n Dine a static Restaurant Website',
-      'image': '../../../assets/imgs/pizza/pizzamenu.JPG'
+      'image': './assets/imgs/pizza/pizzamenu.JPG'
     },
   ]
 
